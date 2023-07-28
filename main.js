7975e6ba4a6ff66d803233060451e51c6db45c1a
enRamirez <ricky.ramirez125042@gmail.com>
2023-07-27 19:03:27 -0500

    finished my fetch project

--- main.js
+++ main.js
@@ -1,42 +1,26 @@
 window.onload = function() {
-    getCocktails()
-  
-  }
-
-
-
-// first, require the dotenv package's config file
-
-
-
-
-const baseAPI= 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-let searchCocktails = document.getElementById('fetch').value
-let fullAPI = baseAPI + searchCocktails
-let cocktail = []
-
-
-
-const getCocktails = () => {
-    fetch(fullAPI)
-	.then(response => response.json())
-	.then(response => console.log(response))
-    .then(data => cocktail = data)
-	.catch(err => console.error(err));
+    const fetchButton = document.getElementById('fetch');
+    // // Event listener for the button click
+    fetchButton.addEventListener('click', getCocktails);
 }
 
+// // first, require the dotenv package's config file
+const baseAPI = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 
+const getCocktails = () => {
+    const fullAPI = baseAPI;
+    fetch(fullAPI)
+        .then(response => response.json())
+        .then(data => {
+            displayDataOnDOM(data);
+        })
+        .catch(err => console.error(err));
+}
 
-
-// const displayPost = () => {
-//     const allDrinks = document.getElementById('alcohols')
-//     arrayOfPosts.map((post, index) => {
-//       const li = document.createElement('li')
-//       const text = document.createTextNode(`#${index}, Title: ${post.title}:  ${post.body}, by user: ${post.userId}`)
-//       li.appendChild(text)
-//       allPosts.append(li)
-//     })
-//   }
+function displayDataOnDOM(data) {
+    const dataContainer = document.getElementById('dataContainer');
+    dataContainer.innerHTML = JSON.stringify(data);
+}
 
 /**
  * sign up for an api key
